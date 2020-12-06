bbff6fee19b09fd10557f8f839df7ef7ebe1f26c
DimaLut <69689216+DimaLut@users.noreply.github.com>
2020-12-06 10:32:59 +0300

    Добавьте файлы проекта.

---
+++ PropertiesProject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ООП
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player(100, 50);
+
+            Renderer renderer = new Renderer();
+            renderer.Render(player);
+
+        }
+    }
+
+    class Player
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Player(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    class Renderer
+    {
+        public void Render(Player player)
+        {
+            Console.SetCursorPosition(player.X, player.Y);
+            Console.Write('@');
+        }
+    }
+}
